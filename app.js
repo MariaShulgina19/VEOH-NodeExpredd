f73ddb737ea9be5fe7ba58b41b3482c787f821f0
Maria Maria <maria.shulgina.fi@gmail.com>
2020-01-17 17:28:59 +0200

    commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req,res,next)=>{
 });
 
 app.get('/', (req, res,next) => {
-    res.send ('Hello World 2')
+    res.send ('Hello World 3')
     //res.write();
     //res.end(); End need to be with write
 });
