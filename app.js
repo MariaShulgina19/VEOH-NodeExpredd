f2a6c0cfe20e3793a7570607c04217c9bcd1c502
Maria Maria <maria.shulgina.fi@gmail.com>
2020-01-17 17:23:50 +0200

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const PORT =process.env.PORT || 8080;
+//same way to pick up express as forex html
+let app = express(); //app =server
+//app.use() use= everything goes  through , listening all
+//app.get()
+//app.post()
+
+//req and res are the same as before, next=continue listen next
+app.use((req,res,next)=>{
+
+    console.log('PATH: '+ req.path);
+    next();
+
+});
+
+app.get('/', (req, res,next) => {
+    res.send ('Hello World 2')
+    //res.write();
+    //res.end(); End need to be with write
+});
+
+app.use((req, res, next)=>{
+    res.status(404)
+    res.send('page not found');
+});
+//shutdouwn ctrl+c
+app.listen(PORT);
