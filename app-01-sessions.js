eeb83b8489c699571e2464cd4ed8db0e7abf43d8
Maria Maria <maria.shulgina.fi@gmail.com>
2020-01-23 19:29:42 +0200

    commit

--- app-01-sessions.js
+++ app-01-sessions.js
@@ -1,127 +1,109 @@
-
 const express = require('express');
-const PORT =process.env.PORT || 8080;
-const body_parser=require('body-parser');
-//same way to pick up express as forex html
+const PORT = process.env.PORT || 8080;
+const body_parser = require('body-parser');
+const session = require('express-session');
 
-const session =require('express-session') // add block 
-let app = express(); //app =server
-//app.use() use= everything goes  through , listening all
-//app.get()
-//app.post()
+let app = express();
 
-app.use(session({  //packeges
-    secret: '1234qwerty' ,//need to generate btter code
+app.use(body_parser.urlencoded({
+    extended: true
+}));
+
+app.use(session({
+    secret: '1234qwerty',
     resave: true,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         maxAge: 1000000
     }
 }));
 
-let users=[];
+let users = [];
 
-app.use(body_parser.urlencoded({
-    extended: true
-
-}));
-
-//req and res are the same as before, next=continue listen next
-app.use((req,res,next)=>{
-
-    console.log('PATH: '+ req.path);
+app.use((req, res, next) => {
+    console.log(`path: ${req.path}`);
     next();
-
 });
-const is_logged_handler = (req, res, next)=>{
 
-    if(!req.session.user) {
-       return res.redirect('/login');
+const is_logged_handler = (req, res, next) => {
+    if (!req.session.user) {
+        return res.redirect('/login');
     }
     next();
 };
 
-
-app.get('/',is_logged_handler, (req, res, next) =>{
-    const user =req.session.user;
+app.get('/', is_logged_handler, (req, res, next) => {
+    const user = req.session.user;
     res.write(`
-    
     <html>
     <body>
         Logged in as user: ${user}
-        <form action ="log out" method "POST">
-        
-        <button type="submit">Log out</button>
-    
-    </body>
+        <form action="/logout" method="POST">
+            <button type="submit">Log out</button>
+        </form>
     </html>
+    </body>
     `);
-res.end();
+    res.end();
 });
 
-app.post('/logout', (req,res, next)=>{
+app.post('/logout', (req, res, next) => {
     req.session.destroy();
     res.redirect('/login');
 });
 
-app.get('/', (req, res,next) => {
-    res.send ('Hello World 3')
-    //res.write();
-    //res.end(); End need to be with write
-});
-
-app.use((req, res, next)=>{
-    res.status(404)
-    res.send('page not found');
-});
-
-app.get('login', (req,res,next) =>{
-    console.log('user:', req.session.user)
+app.get('/login', (req, res, next) => {
+    console.log('user: ', req.session.user)
     res.write(`
-    
     <html>
     <body>
-        <form action ="login" method "POST">
-        <input type="text" name= "user name">
-        <button type="submit">Log in</button>
-    
-      <form action ="register" method "POST">
-        <input type="text" name= "user name">
-        <button type="submit">Register</button>
-    
+        <form action="/login" method="POST">
+            <input type="text" name="user_name">
+            <button type="submit">Log in</button>
+        </form>
+        <form action="/register" method="POST">
+            <input type="text" name="user_name">
+            <button type="submit">Register</button>
+        </form>
     </body>
-    </html>
+    <html>
     `);
-
     res.end();
-    });
-    
-    app.post('/login', (req,res, next)=>{
-         const user_name= req.body.user_name;
-        let user =users.find
-    });
+});
 
-    if(user){
-        console.log('user logged in',user);
-        req.session.user=user;
-        return res.redirect('/login')
-    }
-    console.log('user name is not registere',user);
-    res.redirect('login');
-    app.post('/register', (req,res, next)=>{
-        const user_name= req.body,user_name;
-        users.find((name)=>{
-            return user_name==name;
+app.post('/login', (req, res, next) => {
+    const user_name = req.body.user_name;
+    let user = users.find((name) => {
+        return user_name == name;
     });
-    
-    if(user){
-        return res.send('Use name already registered');
+    if (user) {
+        console.log('User logged in: ', user);
+        req.session.user = user;
+        return res.redirect('/');
     }
-    
+    console.log('User name not registered: ', user);
+    res.redirect('/login');
+});
+
+app.post('/register', (req, res, next) => {
+    const user_name = req.body.user_name;
+    let user = users.find((name) => {
+        return user_name == name;
+    });
+    if (user) {
+        return res.send('User name already registered');
+    }
     users.push(user_name);
     console.log('users:', users);
     res.redirect('/login');
-    });
-    
-//shutdouwn ctrl+c
+});
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.send(`
+        page not found
+    `);
+});
+
+//Shutdown server CTRL + C in terminal
 app.listen(PORT);
