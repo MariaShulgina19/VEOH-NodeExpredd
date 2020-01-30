f0b07147ce7bab39635b5e31a78d5a716da98c33
Maria Maria <maria.shulgina.fi@gmail.com>
2020-01-30 18:13:36 +0200

    commit

--- app-03-sessions-mongo-shoppingList.js
+++ app-03-sessions-mongo-shoppingList.js
@@ -5,18 +5,28 @@ const session = require('express-session');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+const list_schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        //user: user_schema // is it ok?
+    },
+   // childrenProduct_schema :[product_schema] //addig inside 01
+});
 const user_schema = new Schema({
     name: {
         type: String,
         required: true
-    }
+        
+    //shoppinglist:{
+       // type: list_schema,
+       // required: true,
+      //          }
+    },
+    childrenList_schema :[list_schema] //addig inside 30.01
 });
-const list_schema = new Schema({
-    name: {
-        type: String,
-        required: true
-    }
-});
+
 const product_schema = new Schema({
     name: {
         type: String,
@@ -31,6 +41,7 @@ const product_schema = new Schema({
    //     type: jpg,
    //     required: true  
    // }
+   
 });
 
 const user_model = mongoose.model('user', user_schema);
@@ -53,7 +64,7 @@ app.use(session({
 }));
 
 let users = [];
-let lists =[];
+let lists = [];
 //if needed own product list
 let products =[]
 
@@ -71,13 +82,18 @@ const is_logged_handler = (req, res, next) => {
 
 app.get('/', is_logged_handler, (req, res, next) => {
     const user = req.session.user; //written in the page
+   // const list = req.session.list; //added 27.01
+    //trying to print all 
+    
+
     res.write(`
     <html>
     <body>
         Logged in as user: ${user.name} 
+        Your lists: ${user.childrenList_schema} //what it will show?
         
-        <form action="add-list" method="POST">
-        <input type="text" name="list">
+        <form action="/add-list" method="POST">
+        <input type="text" name="list_name">
         <button type="submit" class="add_button">Add list</button>
     </form>
 
@@ -99,9 +115,15 @@ app.post('/logout', (req, res, next) => {
 
 app.get('/login', (req, res, next) => {
     console.log('user: ', req.session.user)
+
+    user_model.find()
+    .then((name) => {
+        console.log(name)
+    });
     res.write(`
     <html>
     <body>
+       Hello ! How are you today?)
         <form action="/login" method="POST">
             <input type="text" name="user_name">
             <button type="submit">Log in</button>
@@ -118,16 +140,21 @@ app.get('/login', (req, res, next) => {
 
 app.post('/login', (req, res, next) => {
     const user_name = req.body.user_name;
+    
     user_model.findOne({
         name: user_name
     }).then((user) => {
         if (user) {
             req.session.user = user;
+    
+            
+           // req.session.list = list;//to check
             return res.redirect('/');
-        }
+                  }
 
         res.redirect('/login');
     });
+    
 });
 
 app.post('/register', (req, res, next) => {
@@ -150,27 +177,50 @@ app.post('/register', (req, res, next) => {
             return res.redirect('/login');
         });
 
+        
+
     });
 });
-app.post('/addnewlist', (req, res, next) => {
+app.post('/add-list', (req, res, next) => {
     const list_name = req.body.list_name;
+    const user = is_logged_handler;
 
-    list_model.findOne({
-        name: list_name
+    //searching in users array
+    //user_model.findOne({
+    //    childrenList_schema: list_name
+
+
+    //user_model.list_model.findOne({
+
+   list_model.findOne({
+       name: list_name
     }).then((list) => {
         if (list) {
             console.log('List name already registered');
-            return res.redirect('/'); //onother way where to redirect
+            return res.redirect('/'); //another way where to redirect
         }
 
         let new_list = new list_model({
-            name: list_name
+        //let new_list = new user.model.list_model({   
+        name: list_name
             
         });
 
+        //new_list.save().then(() => {
+        //    return res.redirect('/');
+        //});
+
+       
+       // user.childrenList_schema.push(new_list);
+       //how to understand is it working or not?
+        user_model.updateOne(
+            {$push:{childrenList_schema: new_list}}
+        );
+
         new_list.save().then(() => {
-            return res.redirect('/');
+           return res.redirect('/');
         });
+        
 
     });
 });
